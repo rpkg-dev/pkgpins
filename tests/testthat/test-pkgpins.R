9b1eb8cfaafd7e2903b8d62280ee2f84215aa941
Salim B <git@salim.space>
2021-12-08 02:47:34 +0100

    minor changes:
    - improve test
    - fix typo
    - update pkgdown site

--- tests/testthat/test-pkgpins.R
+++ tests/testthat/test-pkgpins.R
@@ -3,7 +3,11 @@
 test_that("hash_fn_call() properly evaluates the call's arguments", {
 
   foo <- function(a, b, c) hash_fn_call(from_fn = "foo")
+  hashed <- "foo-780e938ebcc489f7182e21d895d7eae9"
 
   expect_identical(foo(1 + 1, 'ar', list(1, 77)),
-                   foo(3 - 1, paste0("a", "r"), list(a = 1, 77) %>% rlang::set_names(nm = NULL)))
+                   hashed)
+
+  expect_identical(foo(3 - 1, paste0("a", "r"), list(a = 1, 77) %>% rlang::set_names(nm = NULL)),
+                   hashed)
 })
